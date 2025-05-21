4e0d447a445871b2ec4796b5f8a0e1d1916809d8
Anahi Galindo <ganahi859@gmail.com>
2025-05-21 17:10:03 -0600

    agregar tarea 2

--- src/test/java/ejerciciosSelenium/SelectoresRobustosUnicos.java
+++ src/test/java/ejerciciosSelenium/SelectoresRobustosUnicos.java
@@ -15,22 +15,24 @@ public class SelectoresRobustosUnicos {
         //greenkart
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
+        //XPATH
         WebElement searchInp = driver.findElement(By.xpath("//input[@type='search']"));
+        WebElement searchBtn = driver.findElement(By.cssSelector("button.search-button"));
         WebElement linkedText = driver.findElement(By.xpath("//a[text()='Career Focussed QA Meetup with Rahul Shetty @Pune - Limited Seats! Book Now!']"));
         WebElement logoText = driver.findElement(By.xpath("//div[@class='brand greenLogo']"));
         WebElement topDeals = driver.findElement(By.xpath("//a[@href='#/offers']"));
         WebElement flightBooking = driver.findElement(By.xpath("//a[text()='Flight Booking']"));
-
-
-
-
-
-        WebElement cartIcon = driver.findElement(By.cssSelector("a.cart-icon"));
-        WebElement searchBtn = driver.findElement(By.cssSelector("button.search-button"));
+        WebElement BrocolliImg = driver.findElement(By.xpath("//img[@alt='Brocolli - 1 Kg']"));
+        WebElement cartIconByAlt = driver.findElement(By.xpath("//img[@alt='Cart']"));
 
 
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
+        //XPATH
+        WebElement radio2 = driver.findElement(By.xpath("//label[@for='radio2']"));
+        WebElement radio3 = driver.findElement(By.xpath("//label[@for='radio3']"));
+
+        //CSSSELECTOR
         WebElement optionOneCheck = driver.findElement(By.cssSelector("input#checkBoxOption1"));
         WebElement iFrameExampleBtn = driver.findElement(By.cssSelector("iframe#courses-iframe"));
         WebElement selectBtn = driver.findElement(By.cssSelector("select#dropdown-class-example"));
@@ -38,7 +40,9 @@ public class SelectoresRobustosUnicos {
         WebElement option = driver.findElement(By.cssSelector("button#openwindow"));
         WebElement blinkingText = driver.findElement(By.cssSelector("a.blinkingText"));
         WebElement logoImg = driver.findElement(By.cssSelector("img.logoClass"));
-
+        WebElement enterName = driver.findElement(By.cssSelector("input[name='enter-name']"));
+        WebElement alertBtn = driver.findElement(By.cssSelector("input[value='Alert']"));
+        WebElement confirmBtn = driver.findElement(By.cssSelector("input[value='Confirm']"));
 
     }
 }
